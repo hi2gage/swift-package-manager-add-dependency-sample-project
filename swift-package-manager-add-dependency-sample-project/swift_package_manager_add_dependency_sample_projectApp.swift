6c0a6e5823f3be1520cd253c2e44665e99b24d92
hi2gage <hi2gage.pdx@gmail.com>
2024-06-29 23:43:23 -0600

    Initial Commit

---
+++ swift-package-manager-add-dependency-sample-project/swift_package_manager_add_dependency_sample_projectApp.swift
@@ -0,0 +1,10 @@
+import SwiftUI
+
+@main
+struct swift_package_manager_add_dependency_sample_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-package-manager-add-dependency-sample-projectTests/swift_package_manager_add_dependency_sample_projectTests.swift
@@ -0,0 +1,9 @@
+import Testing
+
+struct swift_package_manager_add_dependency_sample_projectTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
